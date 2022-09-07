d9dd618486b94b39500baa9397754320b52e413f
Millenapires32 <millena.silva32@etec.sp.gov.br>
2022-09-07 12:15:44 -0300

    Correção

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -1,15 +1,15 @@
 <?php
 
 //Carregar todas as credenciais do banco de bados 
-$hostname = "localhost:3307";
-$database = "tcc";
-$user = "root";
-$passaword = "usbw";
+$hostname = "";
+$database = "";
+$user = "";
+$passaword = "";
 
 try{
     $pdo = new PDO('mysql:host='.$hostname.';dbname='.$database, $user, $passaword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo 'Conexão com banco de dados '.$database.', foi realizado com sucesso!';
+    echo 'Conexão com banco de dados '.$database.', foi realizado com sucesso!';
 
 } catch(PDOException $e){
     echo 'Erro: '.$e->getMessage();
