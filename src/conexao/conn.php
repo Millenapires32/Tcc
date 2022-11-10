a473d8e630516f3eea0aa5ca70998d2071db5693
Millenapires32 <millena.silva32@etec.sp.gov.br>
2022-11-10 20:06:21 -0300

    adicionando datatables para teste

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -9,7 +9,7 @@ $passaword = "2IGVsO6513";
 try{
     $pdo = new PDO('mysql:host='.$hostname.';dbname='.$database, $user, $passaword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'Conexão com banco de dados '.$database.', foi realizado com sucesso!';
+    //echo 'Conexão com banco de dados '.$database.', foi realizado com sucesso!';
 
 } catch(PDOException $e){
     echo 'Erro: '.$e->getMessage();
